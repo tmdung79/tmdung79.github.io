8ca68a860c2a65a20ca6a40e1ba0ca42c2899d01
tmdung79 <thientinhsu7@gmail.com>
2019-06-26 21:13:17 +0700

    tao hieu ung

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
-﻿ $(function(){
+ $(function(){
+ 	// vitricuaedu = $('#khoiedu').offset().top ;
+ 	// console.log(vitricuaedu)
 
- 
+ 		$(".down,.kn").click(function() {
+ 			$('body,html').animate({ scrollTop:$('#khoiedu').offset().top},800);
+ 			return false ;
+
+ 		})
+ 		$(".about").click(function() {
+ 			$('body,html').animate({ scrollTop:$('#khoiedu').offset().top},800);
+ 			return false ;
+ 			
+ 		})
 })
